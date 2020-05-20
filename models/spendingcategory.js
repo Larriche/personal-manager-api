7ea8498cdeb38ab6e7ca93e5daefa610c7871588
Richman Larry Clifford <richmanlclifford@eziteksolutions.com>
2020-05-20 21:09:44 +0000

    Fixed formatting of spending category model

--- models/spendingcategory.js
+++ models/spendingcategory.js
@@ -1,18 +1,19 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  const SpendingCategory = sequelize.define('SpendingCategory', {
-    name: DataTypes.STRING
-  }, {
-    tableName: 'spending_categories'
-  });
+    const SpendingCategory = sequelize.define('SpendingCategory', {
+        name: DataTypes.STRING
+    }, {
+        tableName: 'spending_categories'
+    });
 
-  SpendingCategory.associate = function(models) {
-    SpendingCategory.belongsTo(models.User, {
-      foreignKey: 'userId',
-      onDelete: 'CASCADE',
-      as: 'user'
-    })
-  };
+    SpendingCategory.associate = function(models) {
+        SpendingCategory.belongsTo(models.User, {
+            foreignKey: 'userId',
+            onDelete: 'CASCADE',
+            as: 'user'
+        })
+    };
 
-  return SpendingCategory;
+    return SpendingCategory;
 };
