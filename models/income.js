5ffdbc945742620856032960e2fd7fa0bf5fe504
Richman Larry Clifford <richmanlclifford@eziteksolutions.com>
2020-05-21 01:41:07 +0000

    Formatted incomes model

--- models/income.js
+++ models/income.js
@@ -1,30 +1,31 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  const Income = sequelize.define('Income', {
-    name: DataTypes.STRING
-  }, {
-    tableName: 'incomes'
-  });
-
-  Income.associate = function(models) {
-    Income.belongsTo(models.User, {
-      foreignKey: 'userId',
-      onDelete: 'CASCADE',
-      as: 'user'
+    const Income = sequelize.define('Income', {
+        name: DataTypes.STRING
+    }, {
+        tableName: 'incomes'
     });
 
-    Income.belongsTo(models.Wallet, {
-      foreignKey: 'walletId',
-      onDelete: 'CASCADE',
-      as: 'wallet'
-    });
+    Income.associate = function(models) {
+        Income.belongsTo(models.User, {
+            foreignKey: 'userId',
+            onDelete: 'CASCADE',
+            as: 'user'
+        });
 
-    Income.belongsTo(models.IncomeSource, {
-      foreignKey: 'incomeSourceId',
-      onDelete: 'CASCADE',
-      as: 'income_source'
-    });
-  };
+        Income.belongsTo(models.Wallet, {
+            foreignKey: 'walletId',
+            onDelete: 'CASCADE',
+            as: 'wallet'
+        });
 
-  return Income;
+        Income.belongsTo(models.IncomeSource, {
+            foreignKey: 'incomeSourceId',
+            onDelete: 'CASCADE',
+            as: 'income_source'
+        });
+    };
+
+    return Income;
 };
