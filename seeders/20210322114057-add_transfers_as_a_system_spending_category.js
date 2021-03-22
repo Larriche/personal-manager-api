6e75f7df07e5c15f218f4766a0939367e9041299
Richman Clifford <richmanlclifford@eziteksolutions.com>
2021-03-22 12:08:58 +0000

    Added seeders to add Transfers as system-default spending and income categories to track inter-wallet transfers

---
+++ seeders/20210322114057-add_transfers_as_a_system_spending_category.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('spending_categories', [
+      {
+        name: 'Transfers',
+        color: '#20F3E',
+        is_system: true,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
