b9138ba4395f76c23274304a6d0e98fd6dcc5b95
Richman Clifford <richmanlclifford@eziteksolutions.com>
2021-03-25 16:55:43 +0000

    Fix auth check to detect when user isn't in db anymore

--- middlewares/verify_authentication.js
+++ middlewares/verify_authentication.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv').config();
 const config = process.env;
 const jwt = require('jsonwebtoken');
+const User = require('./../models').User;
 const secret = config.JWT_SECRET.toString().trim();
 
 const verifyAuthentication = function (request, response, next) {
@@ -13,9 +14,13 @@ const verifyAuthentication = function (request, response, next) {
 
     if (!token) return response.status(401).json({ auth: false, message: 'No token provided.' });
 
-    jwt.verify(token, secret, function (err, decoded) {
+    jwt.verify(token, secret, async function (err, decoded) {
         console.log(err);
         if (err) return response.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
+
+        let user = await User.findOne({ id: decoded.id });
+
+        if (!user) return response.status(401).json({ auth: false, message: 'No user found for this token' });
 
         request.user = {
             id: decoded.id
